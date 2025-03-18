e8bbc028f1c25236970d673d06be5273f6b033cf
Nguyen-DucManh <22024504@vnu.edu.vn>
2025-03-18 16:43:39 +0700

    Initial commit

---
+++ src/test/java/com/example/demoDocker/DemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoDocker;
+
+import hello.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)  // Specify the main application class
+class DemoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
